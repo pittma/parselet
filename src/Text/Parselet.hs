82a2810f530c25476fe36405ea2c06838d7a9b02
dan pittman <dan@dpitt.me>
2024-02-09 16:12:58 -0800

    return errors containing "rest"

--- src/Text/Parselet.hs
+++ src/Text/Parselet.hs
@@ -10,37 +10,37 @@ pattern a :< as <- (uncons -> Just (a, as))
 pattern TEmpty <- (uncons -> Nothing)
 
 newtype Parser a = Parser
-  { parser :: Text -> Maybe (a, Text)
+  { parser :: Text -> Either Text (a, Text)
   }
 
-runParser :: Text -> Parser a -> Maybe (a, Text)
+runParser :: Text -> Parser a -> Either Text (a, Text)
 runParser s p = parser p s
 
 instance Functor Parser where
   fmap f p =
     Parser $ \s ->
       case runParser s p of
-        Nothing -> Nothing
-        Just (result, rest) -> Just (f result, rest)
+        Right (result, rest) -> Right (f result, rest)
+        Left l -> Left l
 
 instance Applicative Parser where
-  pure x = Parser $ \s -> Just (x, s)
+  pure x = Parser $ \s -> Right (x, s)
   pf <*> pa =
     Parser $ \s ->
       case runParser s pf of
-        Nothing -> Nothing
-        Just (f, rest) ->
+        Left l -> Left l
+        Right (f, rest) ->
           case runParser rest pa of
-            Nothing -> Nothing
-            Just (x, rest') -> Just (f x, rest')
+            Left l -> Left l
+            Right (x, rest') -> Right (f x, rest')
 
 instance Monad Parser where
   return = pure
   pa >>= f =
     Parser $ \s ->
       case runParser s pa of
-        Nothing -> Nothing
-        Just (result, rest) -> runParser rest (f result)
+        Left l -> Left l
+        Right (result, rest) -> runParser rest (f result)
 
 assocr :: Parser a -> Parser (a -> a -> a) -> Parser a
 assocr var op = do
@@ -54,30 +54,30 @@ assocr var op = do
         <|> pure x
   
 nop :: Parser ()
-nop = Parser $ \s -> Just ((), s)
+nop = Parser $ \s -> Right ((), s)
 
 fail :: Parser a
-fail = Parser $ const Nothing
+fail = Parser Left
 
 phrase :: Text -> Parser Text
 phrase match = Parser $ \s ->
   if match `isPrefixOf` s
-  then Just (match, drop (length match) s)
-  else Nothing
+  then Right (match, drop (length match) s)
+  else Left s
 
 phrase_ :: Text -> Parser ()
 phrase_ t = void (phrase t)
 
 upto :: Char ->  Parser ()
 upto c = Parser $ \case
-  ss@(s :< _) | s == c -> Just ((), ss)
+  ss@(s :< _) | s == c -> Right ((), ss)
   (_ :< rest) | otherwise -> runParser rest (upto c)
-  _ -> Nothing
+  s -> Left s
 
 one :: Char -> Parser Char
 one c = Parser $ \case
-  (s :< rest) | s == c -> Just (s, rest)
-  _ -> Nothing
+  (s :< rest) | s == c -> Right (s, rest)
+  s -> Left s
 
 one_ :: Char -> Parser ()
 one_ c = void (one c)
@@ -85,29 +85,29 @@ one_ c = void (one c)
 any :: Parser Char
 any =
   Parser $ \case
-    (x :< xs) -> Just (x, xs)
-    TEmpty -> Nothing
+    (x :< xs) -> Right (x, xs)
+    TEmpty -> Left ""
 
 oneOf :: [Char] -> Parser Char
 oneOf cs = Parser $ \case
-  (s :< rest) | s `elem` cs -> Just (s, rest)
-  _ -> Nothing
+  (s :< rest) | s `elem` cs -> Right (s, rest)
+  s -> Left s
 
 alphanumeric :: Parser Char
 alphanumeric = oneOf "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
 
 not :: Char -> Parser Char
 not c =
-  Parser $ \(x :< xs) ->
+  Parser $ \s@(x :< xs) ->
     if x /= c
-      then Just (x, xs)
-      else Nothing
+      then Right (x, xs)
+      else Left s
 
 eof :: Parser Text
 eof =
   Parser $ \case
-    TEmpty -> Just ("", "")
-    _ -> Nothing
+    TEmpty -> Right ("", "")
+    s -> Left s
   
 repeat :: Parser Char -> Parser Text
 repeat p = fmap pack (repeatUntil p)
@@ -121,8 +121,8 @@ whitespace = optional $ void (repeat (oneOf " \n\t"))
 (<|>) :: Parser a -> Parser a -> Parser a
 (<|>) p q = Parser $ \s ->
   case runParser s p of
-    Just (r, rest) -> Just (r, rest)
-    Nothing -> runParser s q
+    Right (r, rest) -> Right (r, rest)
+    _ -> runParser s q
 
 optional :: Parser () -> Parser ()
 optional p = p <|> nop
@@ -145,8 +145,8 @@ takeUntil :: Text -> Parser Text
 takeUntil str =
   Parser $ \s ->
     case go str s of
-      (TEmpty, TEmpty) -> Nothing
-      result -> Just result
+      (TEmpty, TEmpty) -> Left s
+      result -> Right result
   where
     go :: Text -> Text -> (Text, Text)
     go s (ss :< str2)
